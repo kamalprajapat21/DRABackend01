c5285d436dad2bc4409b15d81d66e44390081f6f
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-07-21 16:52:47 +0530

    update code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import { checkuserexistance } from '../controllers/signupotpController.js';
 import { checkuserexistance2 } from '../controllers/loginotpController.js';
-// import { createUserWithFirebaseToken } from '../controllers/signupotpController.js';
+import { createUserWithFirebaseToken } from '../controllers/signupotpController.js';
 import { Signup1,Signup2,Signup3 } from '../controllers/signupdetailsController.js';
 import multer from 'multer';
 import path from 'path';
@@ -42,7 +42,7 @@ router.post('/signup/checkuserexistance', checkuserexistance);
 
 router.post('/login/checkuserexistance2', checkuserexistance2);
 // router.post('/login/check-otp', checkOtp2);
-router.post('/firebase-token', firebaseTokenAuth);
+router.post('/api/auth/signup/create-with-token', createUserWithFirebaseToken);
 
 // router.post('/login/check-otp', checkOtp2);
 
@@ -59,7 +59,7 @@ router.post('/signup2', upload.fields([
   router.post('/signup3', upload.single('uploadbankstatement'), Signup3);
 //serverRoutes
 
-// router.post('/firebase-token', firebaseTokenAuth);
+router.post('/firebase-token', firebaseTokenAuth);
 router.get('/test', (req, res) => res.json({ ok: true }));
 
 export default router;
